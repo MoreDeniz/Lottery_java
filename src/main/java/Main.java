1fd2382536b7f8935f498d16d17d3225c7743be3
Mayya <msalosina@mail.ru>
2023-05-06 20:23:26 +0300

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+        ToyShop toyShop = new ToyShop();
+
+        toyShop.add("Воздушный  змей", 12, 5);
+        toyShop.add("Магнитные шашки", 10, 16);
+        toyShop.add("Лазерная указка", 7, 16);
+        toyShop.add("Пушистый зайчик", 15, 8);
+        toyShop.add("Головоломка Шар", 25, 15);
+        toyShop.add("Кубик - Рубика ", 30, 15);
+        toyShop.add("Мыльные пузырики", 40, 25);
+
+        toyShop.calculate();
+
+        ToysForDraw toysForDraw = new ToysForDraw(toyShop);
+
+        Roulette toysWin = new Roulette(5, toysForDraw);
+        toysWin.winToysList("lots.txt");
+        System.out.println(toysWin);
+
+    }
 }
